f1176683b61cc22226378b9a5229f9ef3aa4a5fb
countgrey <mag.34737@gmail.com>
2024-05-14 15:31:25 +0500

    Это победа

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,98 +1,98 @@
-﻿using AngleSharp.Html.Parser;
-
-namespace German_Project;
+﻿namespace German_Project;
 
 public partial class MainPage : ContentPage
 {
-    private int lessonTimeIndexNow = 0;
-    public List<String[]> todayLessons;
 
     private Timer _timer;
 
-    public DayOfWeek today;
-    public DateTime now;
+    public DayOfWeek today = DayOfWeek.Monday;
+    public DateTime now = DateTime.MaxValue;
 
-    public Dictionary<DayOfWeek, List<DateTime>> ScheduleList;
+    public List<LessonsForDay> schduleForWeek;
+    public LessonsForDay todayLessons;
+    public ScheduleElement lessonOrBreakNow;
+
+    public bool LessonsEndToday = false;
+
+
+    private void updateNowLesson()
+    {
+        if (schduleForWeek != null)
+        {
+            todayLessons = schduleForWeek[(int)today - 1];
+            foreach (var lessonOrBreak in todayLessons.LessonsAndBreaks)
+            {
+                if (now.TimeOfDay < lessonOrBreak.Ending.TimeOfDay && now.TimeOfDay >= lessonOrBreak.Beginning.TimeOfDay)
+                {
+                    lessonOrBreakNow = lessonOrBreak;
+                    break;
+                }
+                else
+                {
+                    lessonOrBreakNow = new Lesson() { Index = todayLessons.LessonsAndBreaks[0].Index,
+                                                      Room = ((Lesson)todayLessons.LessonsAndBreaks[0]).Room,
+                                                      Ending = todayLessons.LessonsAndBreaks[0].Beginning,
+                                                      Name = "Пары еще не начались"};
+                }  
+            }
+
+            LessonsEndToday = todayLessons.LessonsAndBreaks.Last().Ending.TimeOfDay < now.TimeOfDay;
+
+        }
+        
+    }
 
     [Obsolete]
     private async void TimerCallback(object state)
     {
         // Здесь можно разместить код, который нужно выполнить каждую секунду
+
         today = DateTimeOffset.Now.DayOfWeek;
         now = DateTimeOffset.Now.LocalDateTime;
+        
+        updateNowLesson();
+        
+
+
         await Device.InvokeOnMainThreadAsync(() =>
         {
-            endsIn.Text = TimeUntilEndOfClass(today, now);
-
-            for (int i = 0; i < MainStackForSchedule.Count; i++)
+            //lessonName.Text = $"{lessonOrBreakNow.Index}";
+            if (lessonOrBreakNow != null)
             {
-                ((Frame)MainStackForSchedule.Children[i]).BackgroundColor = Color.FromHex("#502bd4");
-            }
+                if (!LessonsEndToday)
+                {
+                    endsIn.Text = $"Осталось {lessonOrBreakNow.Ending.TimeOfDay - now.TimeOfDay:hh\\:mm\\:ss}";
+                    if (lessonOrBreakNow is Break)
+                        lessonName.Text = $"Перемена {((Lesson)todayLessons.LessonsAndBreaks[todayLessons.LessonsAndBreaks.IndexOf(lessonOrBreakNow) + 1]).Room}";
+                    else 
+                        lessonName.Text = $"{lessonOrBreakNow.Index}. {((Lesson)lessonOrBreakNow).Name}. {((Lesson)lessonOrBreakNow).Room}";
+                }
+                else lessonName.Text = "Пары на сегодня закончились";
 
-            if (lessonTimeIndexNow/2 - 2 >= 0 && lessonTimeIndexNow / 2 - 2 < MainStackForSchedule.Children.Count)
-                ((Frame)MainStackForSchedule.Children[lessonTimeIndexNow/2 - 2]).BackgroundColor = Color.FromHex("#802bd4");
-            //Console.WriteLine(lessonTimeIndexNow);
+                foreach (var lesson in MainStackForSchedule.Children)
+                {
+                    ((Frame)lesson).BackgroundColor = Color.FromHex("#502bd4");
+                }
+                
+                if (todayLessons.LessonsAndBreaks.Contains(lessonOrBreakNow))
+                    ((Frame)MainStackForSchedule.Children[todayLessons.LessonsAndBreaks.IndexOf(lessonOrBreakNow)]).BackgroundColor = Color.FromHex("#802bd4");
+
+            }
         });
     }
 
     public MainPage()
     {
-        ScheduleList = new();
-        List<DateTime> mondaySchedule = new List<DateTime>
-        {
-            new DateTime(1, 1, 1, 8, 30, 0),
-            new DateTime(1, 1, 1, 9, 15, 0),
-            new DateTime(1, 1, 1, 9, 20, 0),
-            new DateTime(1, 1, 1, 10, 50, 0),
-            new DateTime(1, 1, 1, 11, 10, 0),
-            new DateTime(1, 1, 1, 12, 40, 0),
-            new DateTime(1, 1, 1, 12, 50, 0),
-            new DateTime(1, 1, 1, 14, 20, 0),
-            new DateTime(1, 1, 1, 14, 35, 0),
-            new DateTime(1, 1, 1, 16, 5, 0),
-            new DateTime(1, 1, 1, 16, 10, 0),
-            new DateTime(1, 1, 1, 17, 40, 0),
-            new DateTime(1, 1, 1, 17, 45, 0),
-            new DateTime(1, 1, 1, 19, 15, 0)
-        };
-        ScheduleList.Add(DayOfWeek.Monday, mondaySchedule);
-
-        for (DayOfWeek day = DayOfWeek.Tuesday; day <= DayOfWeek.Saturday; day++)
-        {
-            if (day != DayOfWeek.Sunday)
-            {
-                List<DateTime> schedule = new List<DateTime>
-            {
-                new DateTime(1, 1, 1, 8, 30, 0),
-                new DateTime(1, 1, 1, 10, 00, 0),
-                new DateTime(1, 1, 1, 10, 10, 0),
-                new DateTime(1, 1, 1, 11, 40, 0),
-                new DateTime(1, 1, 1, 12, 0, 0),
-                new DateTime(1, 1, 1, 13, 30, 0),
-                new DateTime(1, 1, 1, 14, 00, 0),
-                new DateTime(1, 1, 1, 15, 30, 0),
-                new DateTime(1, 1, 1, 15, 40, 0),
-                new DateTime(1, 1, 1, 17, 10, 0),
-                new DateTime(1, 1, 1, 17, 15, 0),
-                new DateTime(1, 1, 1, 18, 45, 0),
-            };
-                ScheduleList.Add(day, schedule);
-            }
-        }
-
         InitializeComponent();
         // Создаем таймер с интервалом в 1 секунду
         _timer = new Timer(TimerCallback, null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
 
-
+        
         string group = Preferences.Get("group", "no");
-
         if (group != "no")
-            UpdateSchedule(group);
+            updateSchedule(group);
     }
 
-
-    
 
     private async void updateButton(object sender, EventArgs e)
     {
@@ -101,167 +101,64 @@ public partial class MainPage : ContentPage
         {
             //в кеш
             Preferences.Set("group", result);
-
-            MainStackForSchedule.Clear();
-            UpdateSchedule(result);
+            updateSchedule(result);
         }
+
     }
 
 
-    private async Task<List<List<string[]>>> LoadAndParseData(string group)
+    private async void updateSchedule(string group)
     {
-        try
+        MainStackForSchedule.Clear();
+
+        schduleForWeek = await ScheduleUpdateFunctions.ParseScheduleForWeek(group);
+        updateNowLesson();
+
+        renderSchedule();
+
+    }
+
+    public void renderSchedule()
+    {
+        foreach (var lesson in todayLessons.LessonsAndBreaks)
         {
-            // URL сайта для парсинга
-            string url = $"https://oksei.ru/studentu/raspisanie_uchebnykh_zanyatij?group={group}";
+            // Получение времени начала и окончания пары
+            DateTime lessonStart = lesson.Beginning;
+            DateTime lessonEnd = lesson.Ending;
 
-            // Создание HTTP-клиента
-            using var httpClient = new HttpClient();
+            // Форматирование времени в строку в нужном формате
+            string lessonTime = lessonStart.ToString("HH:mm") + " - " + lessonEnd.ToString("HH:mm");
 
-            // Получение HTML-страницы
-            var response = await httpClient.GetAsync(url);
-            var htmlContent = await response.Content.ReadAsStringAsync();
-
-            // Создание парсера AngleSharp
-            var parser = new HtmlParser();
-
-            // Парсинг HTML-кода
-            var document = parser.ParseDocument(htmlContent);
-            // Извлечение данных из таблицы расписания
-            var scheduleTable = document.QuerySelector("table.table");
-            var rows = scheduleTable.QuerySelectorAll("tbody > tr");
-
-            // Создание списка для хранения расписания
-            var scheduleList = new List<List<string[]>>();
-
-            // Итерация по строкам таблицы
-            foreach (var row in rows)
+            // Создание рамки для отдельного урока
+            Frame lessonFrame = new Frame
             {
-                var lessonsOfDayString = row.QuerySelectorAll("td > ol > li");
-                List<string[]> lessonsOfDayList = new List<string[]>();
-                
-                for (int lessonIndex = 0;  lessonIndex < lessonsOfDayString.Length; lessonIndex++)
+                CornerRadius = 15,
+                Margin = new Thickness(0, 0, 0, 0),
+                BackgroundColor = Color.FromHex("#502bd4"),
+                BorderColor = Color.FromHex("#502bd4")
+            };
+
+            // Создание стека для урока
+            StackLayout lessonStack = new StackLayout();
+
+            if (lesson.Index < 0) // Перемена
+            {
+                lessonStack.Children.Add(new Label
                 {
-                    // Разделяем строку на слова
-                    string[] words =lessonsOfDayString[lessonIndex].TextContent.Trim().Split(' ');
-
-                    // Получаем название пары
-                    string lessonName = string.Join(" ", words.Take(words.Length - 5));
-
-                    // Получаем преподавателя
-                    string teacher = string.Join(" ", words.Skip(words.Length - 5).Take(2));
-
-                    // Получаем аудиторию
-                    string classroom = string.Join(" ", words.Skip(words.Length - 3)).Replace("Аудитория", "");
-
-                    
-
-                    string[] lesson = [(lessonIndex+1).ToString(),
-                        lessonName, teacher, classroom];
-                    lessonsOfDayList.Add(lesson);
-                }
-                
-                scheduleList.Add(lessonsOfDayList);
+                    Text = $"Перемена {lesson.Duration:mm} мин",
+                    FontFamily = "Open Sans MS",
+                    FontAttributes = FontAttributes.Bold,
+                    TextColor = Color.FromHex("#f0f8ff"),
+                    Margin = new Thickness(0, 0, 0, 0)
+                });
             }
 
-            
-
-            // Возвращение списка расписания
-            return scheduleList;
-        }
-        catch (Exception ex)
-        {
-            // Обработка ошибок, если таковые возникнут
-            Console.WriteLine("Ошибка при загрузке и парсинге данных: " + ex.Message);
-            return null;
-        }
-    }
-
-
-    public string TimeUntilEndOfClass(DayOfWeek today, DateTime now)
-    {
-
-        if (ScheduleList.ContainsKey(today))
-        {
-            List<DateTime> schedule = ScheduleList[today];
-
-            foreach (var classStartTime in schedule)
+            else if (lesson.Index >= 0)
             {
-                if (classStartTime.TimeOfDay > now.TimeOfDay)
-                {
-                    lessonTimeIndexNow = schedule.IndexOf(classStartTime);
-
-                    if (lessonTimeIndexNow % 2 == 0) lessonName.Text = "Перемена";
-                    else if (todayLessons != null)
-                    {
-                        if (lessonTimeIndexNow / 2 < todayLessons.Count)
-                        {
-                            lessonName.Text = $"{todayLessons[lessonTimeIndexNow/2][1]}";
-                        }
-
-                        else lessonName.Text = "bimbimbambam";
-                    }
-                    TimeSpan timeUntilNextClass = classStartTime.TimeOfDay - now.TimeOfDay;
-                    return $"{timeUntilNextClass:hh\\:mm\\:ss} осталось";
-                }
-
-            }
-
-            lessonName.Text = "Пары закончились";
-            return "";
-
-        }
-        lessonName.Text = "Сегодня пар нет";
-        return "";
-
-    }
-
-    public async void UpdateSchedule(string group)
-    {
-        List<List<string[]>> lessonsOfWeek = await LoadAndParseData(group);
-        todayLessons = lessonsOfWeek[(int)today-1];
-        // Удаление пустых элементов с конца списка
-        while (todayLessons.Last()[1] == "")
-        {
-            todayLessons.RemoveAt(todayLessons.Count - 1);
-        }
-
-        // Удаление пустых элементов с начала
-
-        for (int les = 0; les < todayLessons.Count; les++)
-        {
-            if (todayLessons[les][1] == "") todayLessons.RemoveAt(les);
-        }
-
-        //var tomorrowLessons = lessonsOfWeek[1];
-
-        foreach (var lesson in todayLessons)
-        {
-            if (lesson[1] != "")
-            {
-                // Получение времени начала и окончания пары
-                DateTime lessonStart = ScheduleList[today][(int.Parse(lesson[0]) * 2) - 2];
-                DateTime lessonEnd = ScheduleList[today][(int.Parse(lesson[0]) * 2) - 1];
-
-                // Форматирование времени в строку в нужном формате
-                string lessonTime = lessonStart.ToString("HH:mm") + " - " + lessonEnd.ToString("HH:mm");
-
-                // Создание рамки для отдельного урока
-                Frame lessonFrame = new Frame
-                {
-                    CornerRadius = 15,
-                    Margin = new Thickness(0, 0, 0, 0),
-                    BackgroundColor = Color.FromHex("#502bd4"),
-                    BorderColor = Color.FromHex("#502bd4")
-                };
-
-                // Создание стека для урока
-                StackLayout lessonStack = new StackLayout();
-
                 // Добавление индекса и названия урока
                 lessonStack.Children.Add(new Label
                 {
-                    Text = lesson[0] + ". " + lesson[1], // Индекс и название
+                    Text = $"{lesson.Index}. {((Lesson)lesson).Name}", // Индекс и название
                     FontFamily = "Open Sans MS",
                     FontAttributes = FontAttributes.Bold,
                     TextColor = Color.FromHex("#f0f8ff"),
@@ -271,7 +168,7 @@ public partial class MainPage : ContentPage
                 // Добавление преподавателя и аудитории
                 lessonStack.Children.Add(new Label
                 {
-                    Text = lesson[2] + ", " + lesson[3], // Преподаватель и аудитория
+                    Text = ((Lesson)lesson).Teacher + ", " + ((Lesson)lesson).Room, // Преподаватель и аудитория
                     FontFamily = "Open Sans MS",
                     FontAttributes = FontAttributes.Bold,
                     TextColor = Color.FromHex("#a9a9a9")
@@ -286,49 +183,13 @@ public partial class MainPage : ContentPage
                     TextColor = Color.FromHex("#a9a9a9")
                 });
 
-                // Установка содержимого рамки
-                lessonFrame.Content = lessonStack;
 
-                // Добавление рамки в основной стек расписания
-                MainStackForSchedule.Children.Add(lessonFrame);
-
-                // Проверяем, не последний ли это урок для группы
-                if (lesson != todayLessons.Last())
-                {
-                    // Добавление рамки с информацией о перерыве только если это не последний урок для группы
-
-                    // Добавление рамки с информацией о перерыве
-                    Frame breakFrame = new Frame
-                    {
-                        BackgroundColor = Color.FromHex("#502bd4"),
-                        CornerRadius = 15,
-                        BorderColor = Color.FromHex("#502bd4"),
-                        Padding = new Thickness(20, 0, 0, 0),
-                        Margin = new Thickness(0, 0, 5, 0)
-                    };
-
-                    // Получение времени начала и окончания перемены
-                    DateTime breakStart = ScheduleList[today][(int.Parse(lesson[0]) * 2) - 1];
-                    DateTime breakEnd = ScheduleList[today][(int.Parse(lesson[0]) * 2)];
-
-                    // Вычисление продолжительности перемена в минутах
-                    int breakDurationMinutes = (int)(breakEnd - breakStart).TotalMinutes;
-
-                    // Создание строки с использованием интерполяции строк
-                    string breakText = $"Перемена {breakDurationMinutes} мин";
-
-                    breakFrame.Content = new Label
-                    {
-                        Text = breakText, // Информация о перерыве
-                        FontFamily = "Open Sans MS",
-                        FontAttributes = FontAttributes.Bold,
-                        TextColor = Color.FromHex("#a9a9a9")
-                    };
-
-                    // Добавление рамки с перерывом в основной стек расписания
-                    MainStackForSchedule.Children.Add(breakFrame);
-                }
             }
+
+            // Добавление рамки в основной стек расписания
+            lessonFrame.Content = lessonStack;
+            MainStackForSchedule.Children.Add(lessonFrame);
+
         }
     }
 }
---
+++ Scripts/ScheduleUpdateFunctions.cs
@@ -0,0 +1,210 @@
+﻿using AngleSharp.Html.Parser;
+
+namespace German_Project;
+
+public class ScheduleElement
+{
+    public int Index { get; set; }
+    public DateTime Beginning { get; set; }
+    public DateTime Ending { get; set; }
+    public TimeSpan Duration { get; set; }
+}
+
+public class Lesson : ScheduleElement
+{
+    public string Name { get; set; }
+    public string Teacher { get; set; }
+    public string Room { get; set; }
+}
+
+public class Break : ScheduleElement
+{
+    public Break(int index)
+    {
+        Index = index;
+    }
+}
+
+public class LessonsForDay
+{
+    public List<ScheduleElement> LessonsAndBreaks { get; set; }
+    public DayOfWeek DayOfWeek { get; set; }
+
+    public void AddTimeForLessons(DayOfWeek dayOfWeek)
+    {
+        DayOfWeek = dayOfWeek;
+        if (DayOfWeek == DayOfWeek.Monday)
+        {
+            LessonsAndBreaks[0].Beginning = new DateTime(1, 1, 1, 8, 30, 0);
+            LessonsAndBreaks[0].Ending = new DateTime(1, 1, 1, 9, 15, 0);
+            LessonsAndBreaks[1].Beginning = new DateTime(1, 1, 1, 9, 15, 0);
+            LessonsAndBreaks[1].Ending = new DateTime(1, 1, 1, 9, 20, 0);
+            LessonsAndBreaks[2].Beginning = new DateTime(1, 1, 1, 9, 20, 0);
+            LessonsAndBreaks[2].Ending = new DateTime(1, 1, 1, 10, 50, 0);
+            LessonsAndBreaks[3].Beginning = new DateTime(1, 1, 1, 10, 50, 0);
+            LessonsAndBreaks[3].Ending = new DateTime(1, 1, 1, 11, 10, 0);
+            LessonsAndBreaks[4].Beginning = new DateTime(1, 1, 11, 10, 0, 0);
+            LessonsAndBreaks[4].Ending = new DateTime(1, 1, 1, 12, 40, 0);
+            LessonsAndBreaks[5].Beginning = new DateTime(1, 1, 1, 12, 40, 0);
+            LessonsAndBreaks[5].Ending = new DateTime(1, 1, 1, 12, 50, 0);
+            LessonsAndBreaks[6].Beginning = new DateTime(1, 1, 1, 12, 50, 0);
+            LessonsAndBreaks[6].Ending = new DateTime(1, 1, 1, 14, 20, 0);
+            LessonsAndBreaks[7].Beginning = new DateTime(1, 1, 1, 14, 20, 0);
+            LessonsAndBreaks[7].Ending = new DateTime(1, 1, 1, 14, 35, 0);
+            LessonsAndBreaks[8].Beginning = new DateTime(1, 1, 1, 14, 35, 0);
+            LessonsAndBreaks[8].Ending = new DateTime(1, 1, 1, 16, 05, 0);
+            LessonsAndBreaks[9].Beginning = new DateTime(1, 1, 1, 16, 05, 0);
+            LessonsAndBreaks[9].Ending = new DateTime(1, 1, 1, 16, 10, 0);
+            LessonsAndBreaks[10].Beginning = new DateTime(1, 1, 1, 16, 10, 0);
+            LessonsAndBreaks[10].Ending = new DateTime(1, 1, 1, 17, 40, 0);
+            LessonsAndBreaks[11].Beginning = new DateTime(1, 1, 1, 17, 40, 0);
+            LessonsAndBreaks[11].Ending = new DateTime(1, 1, 1, 17, 45, 0);
+            LessonsAndBreaks[12].Beginning = new DateTime(1, 1, 1, 17, 45, 0);
+            LessonsAndBreaks[12].Ending = new DateTime(1, 1, 1, 19, 15, 0);
+        }
+
+        else
+        {
+            LessonsAndBreaks[0].Beginning = new DateTime(1, 1, 1, 8, 30, 0);
+            LessonsAndBreaks[0].Ending = new DateTime(1, 1, 1, 10, 0, 0);
+            LessonsAndBreaks[1].Beginning = new DateTime(1, 1, 1, 10, 0, 0);
+            LessonsAndBreaks[1].Ending = new DateTime(1, 1, 1, 10, 10, 0);
+            LessonsAndBreaks[2].Beginning = new DateTime(1, 1, 1, 10, 10, 0);
+            LessonsAndBreaks[2].Ending = new DateTime(1, 1, 1, 11, 40, 0);
+            LessonsAndBreaks[3].Beginning = new DateTime(1, 1, 1, 11, 40, 0);
+            LessonsAndBreaks[3].Ending = new DateTime(1, 1, 1, 12, 0, 0);
+            LessonsAndBreaks[4].Beginning = new DateTime(1, 1, 1, 12, 0, 0);
+            LessonsAndBreaks[4].Ending = new DateTime(1, 1, 1, 13, 30, 0);
+            LessonsAndBreaks[5].Beginning = new DateTime(1, 1, 1, 13, 30, 0);
+            LessonsAndBreaks[5].Ending = new DateTime(1, 1, 1, 14, 0, 0);
+            LessonsAndBreaks[6].Beginning = new DateTime(1, 1, 1, 14, 0, 0);
+            LessonsAndBreaks[6].Ending = new DateTime(1, 1, 1, 15, 30, 0);
+            LessonsAndBreaks[7].Beginning = new DateTime(1, 1, 1, 15, 30, 0);
+            LessonsAndBreaks[7].Ending = new DateTime(1, 1, 1, 15, 40, 0);
+            LessonsAndBreaks[8].Beginning = new DateTime(1, 1, 1, 15, 40, 0);
+            LessonsAndBreaks[8].Ending = new DateTime(1, 1, 1, 17, 10, 0);
+            LessonsAndBreaks[9].Beginning = new DateTime(1, 1, 1, 17, 10, 0);
+            LessonsAndBreaks[9].Ending = new DateTime(1, 1, 1, 17, 15, 0);
+            LessonsAndBreaks[10].Beginning = new DateTime(1, 1, 1, 17, 15, 0);
+            LessonsAndBreaks[10].Ending = new DateTime(1, 1, 1, 18, 45, 0);
+
+        }
+        foreach (var lesson in LessonsAndBreaks)
+        {
+            lesson.Duration = lesson.Ending.TimeOfDay - lesson.Beginning.TimeOfDay;
+        }
+    }
+}
+
+
+public class ScheduleUpdateFunctions
+{
+    private static DayOfWeek[] DaysOfWorkingWeek = { DayOfWeek.Monday,
+                                                     DayOfWeek.Tuesday,
+                                                     DayOfWeek.Wednesday,
+                                                     DayOfWeek.Thursday,
+                                                     DayOfWeek.Friday,
+                                                     DayOfWeek.Saturday };
+
+    private static DateTime[] TimeSchedule;
+
+    // Парсинг расписания с сайта шараги
+    public async static Task<List<LessonsForDay>> ParseScheduleForWeek(string group)
+    {
+
+        // URL сайта для парсинга
+        string url = $"https://oksei.ru/studentu/raspisanie_uchebnykh_zanyatij?group={group}";
+
+        // Создание HTTP-клиента
+        using var httpClient = new HttpClient();
+
+        // Получение HTML-страницы
+        var response = await httpClient.GetAsync(url);
+        var htmlContent = await response.Content.ReadAsStringAsync();
+
+        // Создание парсера AngleSharp
+        var parser = new HtmlParser();
+
+        // Парсинг HTML-кода
+        var document = parser.ParseDocument(htmlContent);
+        // Извлечение данных из таблицы расписания
+        var scheduleTable = document.QuerySelector("table.table");
+        var rows = scheduleTable.QuerySelectorAll("tbody > tr");
+
+        // Создание списка для хранения расписания
+        var scheduleListForWeek = new List<LessonsForDay>();
+
+        // Итерация по строкам таблицы
+        for (int day = 0; day < 6; day++)
+        {
+            var lessonsOfDayString = rows[day].QuerySelectorAll("td > ol > li");
+            LessonsForDay lessonsForDay = new LessonsForDay();
+            lessonsForDay.LessonsAndBreaks = new();
+            var currentDay = DaysOfWorkingWeek[day];
+
+
+            if (currentDay == DayOfWeek.Monday)
+            {
+                Lesson shittyLesson = new Lesson();
+                shittyLesson.Name = "Разговры о важном";
+                shittyLesson.Room = string.Join(" ", lessonsOfDayString[0].TextContent.Trim().Split(' ').Skip(lessonsOfDayString[0].TextContent.Trim().Split(' ').Length - 3)).Replace("Аудитория", "");
+                shittyLesson.Index = 0;
+                lessonsForDay.LessonsAndBreaks.Add(shittyLesson);
+                lessonsForDay.LessonsAndBreaks.Add(new Break(-1));
+            }
+
+            for (int lessonIndex = 0; lessonIndex < lessonsOfDayString.Length; lessonIndex++)
+            {
+                Lesson lesson = new Lesson();
+                // Разделяем строку на слова
+                string[] words = lessonsOfDayString[lessonIndex].TextContent.Trim().Split(' ');
+
+                lesson.Index = lessonIndex + 1;
+
+                // Получаем название пары
+                lesson.Name = string.Join(" ", words.Take(words.Length - 5));
+
+                // Получаем преподавателя
+                lesson.Teacher = string.Join(" ", words.Skip(words.Length - 5).Take(2));
+
+                // Получаем аудиторию
+                lesson.Room = string.Join(" ", words.Skip(words.Length - 3)).Replace("Аудитория", "");
+
+                lesson.Duration = new TimeSpan();
+
+                lessonsForDay.LessonsAndBreaks.Add(lesson);
+                if (lessonIndex < lessonsOfDayString.Length - 1)
+                    lessonsForDay.LessonsAndBreaks.Add(new Break((lessonIndex+1)*-1));
+            }
+            lessonsForDay.AddTimeForLessons(currentDay);
+
+            for (int i = lessonsForDay.LessonsAndBreaks.Count - 1; i >= 0; i--)
+            {
+                if (lessonsForDay.LessonsAndBreaks[i] is Lesson lesson && lesson.Room.Length < 6)
+                {
+                    lessonsForDay.LessonsAndBreaks.RemoveAt(i); // Удаляем урок
+
+                    // Проверяем, что после удаления урока есть элемент с индексом i
+                    // И что этот элемент - это перемена (Break)
+                    if (i < lessonsForDay.LessonsAndBreaks.Count && lessonsForDay.LessonsAndBreaks[i] is Break)
+                    {
+                        lessonsForDay.LessonsAndBreaks.RemoveAt(i); // Удаляем перемену
+                    }
+                }
+            }
+
+            if (lessonsForDay.LessonsAndBreaks.Last() is Break)
+            {
+                lessonsForDay.LessonsAndBreaks.RemoveAt(lessonsForDay.LessonsAndBreaks.Count - 1);
+            }
+
+            scheduleListForWeek.Add(lessonsForDay);
+        }
+
+
+
+        // Возвращение списка расписания
+        return scheduleListForWeek;
+
+    }
+}
